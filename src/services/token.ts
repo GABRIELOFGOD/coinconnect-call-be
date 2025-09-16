c29dbed01fc549582ca903c6a2b9d203e2558942
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2025-09-16 13:26:38 +0100

    GREAT IS THY FAITHFULNESS LORD UNTO ME!

--- src/services/token.ts
+++ src/services/token.ts
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/env";
 
 export const createToken = (id: string, email: string) => {
-  const token = jwt.sign({ id, email }, JWT_SECRET!, { expiresIn: "7d" });
+  const token = jwt.sign({ id, email }, "mysecureverysecuresecret", { expiresIn: "7d" });
   return token;
 }
 
 export const verifyToken = (token: string) => {
   try {
-    const verified = jwt.verify(token, JWT_SECRET!);
+    const verified = jwt.verify(token, "mysecureverysecuresecret");
     return verified;
   } catch (error) {
     throw error;
